398daa69986ed441595f4c317eb46d60567d97ce
Ahmad Rio <ahmadrio28asia@gmail.com>
2021-07-03 04:19:34 +0700

    add migration & seeder integration

---
+++ database/seeders/20210702203847-user-seeder.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('users', [
+      {
+        name: 'Admin',
+        email: 'admin@admin.com',
+        password: '$2y$10$uzigplV1TcpJ9TH7pc82rel79RXMScxCCRcndImjgmeljxkxH.LEG', // password
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('users', null, {});
+  }
+};
